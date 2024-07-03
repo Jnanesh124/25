807805f208189147e41c5f2f33761f94244db596
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-03 19:50:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6331847574"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://mangodb:mangodb@cluster0.dskkdky.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ottmovies:ottmovies@cluster0.bejrjkt.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Shortner (token system) 
